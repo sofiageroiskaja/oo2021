9008a0061f6bef8f624cdc61c792d635dacee847
sofiageroiskaja <sgeroyskaya@bk.ru>
2021-04-29 23:28:35 +0300

    todo

--- w10_tunnirakenduse_lahendus/demo 2/src/main/java/com/example/demo/entities/Todo.java
+++ w10_tunnirakenduse_lahendus/demo 2/src/main/java/com/example/demo/entities/Todo.java
@@ -12,20 +12,19 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
-    @NotBlank(message = "Must enter a title")
-    private String title;
     
     @NotBlank(message = "Must enter a description")
     private String description;
+
+    private String deadline;
     
     public Todo(){
 
     }
 
-    public Todo(String title, String description){
-        this.title = title;
+    public Todo(String description, String deadline){
         this.description = description;
+        this.deadline = deadline;
     }
 
     public long getId() {
@@ -36,14 +35,6 @@ public class Todo {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDescription() {
         return description;
     }
@@ -52,9 +43,17 @@ public class Todo {
         this.description = description;
     }
 
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
     @Override
     public String toString() {
-        return "Todo{" + "id=" + id + ", title=" + title + ", description=" + description + '}';
+        return "Todo{" + "id=" + id + ", description=" + description + ", deadline=" + deadline + '}';
     }
 
 }
