123d11832c9e4d6d8fb35175a0a44c2f5b2bca03
sofiageroiskaja <sgeroyskaya@bk.ru>
2021-05-07 22:02:54 +0300

    teine punkt peaaegu tehtud

--- w13_kontrolltoo2/LastLetter.java
+++ w13_kontrolltoo2/LastLetter.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.io.*;
  
 public class LastLetter {
@@ -8,23 +11,31 @@ public class LastLetter {
     File file = new File("tekst.txt");
     Scanner input = new Scanner(file); 
     int count = 0;
-    ArrayList last_ch = new ArrayList<>();
-  
-    //esimene punkt
+    ArrayList<Character> last_ch = new ArrayList<Character>();
+
+    //Leian sõnade viimased tähed ja lisan neid arraylisti
     while(input.hasNext()) {
       String word  = input.next();
       char last  = word.charAt(word.length()-1);
-    if(last == '.' || last == ',' || last == '!' || last == '?'){
-        System.out.println(word.charAt(word.length()-2));
+    if(last == '.' || last == ',' || last == '!' || last == '?' || last == '"'){
         last_ch.add(word.charAt(word.length()-2));
     }else{
-        System.out.println(last);
         last_ch.add(last);
     }
     count = count + 1;
     }
-    System.out.println("Letters: " + count + "\n\n");
+    //Trükin välja sõnade viimased tähed (arraylist)
+    System.out.println("Tähed:");
     System.out.println(last_ch);
+    //Trükin välja letters cont (seda polnud ülesandes, tegin lihtsalt enda jaoks)
+    System.out.println("Tähtede arv: " + count);
+
+    //Leian erinevate sõnalõputähtede esinemissagedust ja trükin neid esinemissageduse järjekorras
+    System.out.println("Sõnalõputähtede esinemissagedus:");
+    Set<Character> uniguech = new HashSet<Character>(last_ch);
+    for(Character i : uniguech){
+      System.out.println(i + ": " + Collections.frequency(last_ch, i));
+    }
     input.close();
     }
 }
