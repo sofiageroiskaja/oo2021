34e16a72feae3d84c7860c0c92ce7b03241f0450
sofiageroiskaja <sgeroyskaya@bk.ru>
2021-05-07 14:33:42 +0300

    esimene punkt tehtud

--- w13_kontrolltoo2/LastLetter.java
+++ w13_kontrolltoo2/LastLetter.java
@@ -1,17 +1,24 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
-public class LastLetter{
-    public static void main(String[] args) throws IOException{
-        File file = new File("tekst.txt");
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
-
-        String line = br.readLine();
-        System.out.println(line);
-
-        br.close();
+import java.util.Scanner;
+import java.io.*;
+ 
+public class LastLetter {
+  public static void main(String[] args) throws IOException {
+ 
+    File file = new File("tekst.txt");
+    Scanner input = new Scanner(file); 
+ 
+    int count = 0;
+    while(input.hasNext()) {
+      String word  = input.next();
+      char last  = word.charAt(word.length()-1);
+    if(last == '.' || last == ',' || last == '!' || last == '?'){
+        System.out.println(word.charAt(word.length()-2));
+    }else{
+        System.out.println(last);
     }
+    count = count + 1;
+    }
+    System.out.println("Letters: " + count);
+    input.close();
+  }
 }
