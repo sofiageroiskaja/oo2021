38d1ccb9b169a4839d3937d5252fbf16189ea26a
sofiageroiskaja <sgeroyskaya@bk.ru>
2021-05-07 15:53:55 +0300

    teine punkt pooleli

--- w13_kontrolltoo2/LastLetter.java
+++ w13_kontrolltoo2/LastLetter.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
  
@@ -6,19 +7,24 @@ public class LastLetter {
  
     File file = new File("tekst.txt");
     Scanner input = new Scanner(file); 
- 
     int count = 0;
+    ArrayList last_ch = new ArrayList<>();
+  
+    //esimene punkt
     while(input.hasNext()) {
       String word  = input.next();
       char last  = word.charAt(word.length()-1);
     if(last == '.' || last == ',' || last == '!' || last == '?'){
         System.out.println(word.charAt(word.length()-2));
+        last_ch.add(word.charAt(word.length()-2));
     }else{
         System.out.println(last);
+        last_ch.add(last);
     }
     count = count + 1;
     }
-    System.out.println("Letters: " + count);
+    System.out.println("Letters: " + count + "\n\n");
+    System.out.println(last_ch);
     input.close();
-  }
+    }
 }
