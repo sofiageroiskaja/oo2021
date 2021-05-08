f5271d4c505772a79e2d9c9cb1215fd72e5dbff7
sofiageroiskaja <sgeroyskaya@bk.ru>
2021-05-08 20:12:57 +0300

    teine punkt tehtud

--- w13_kontrolltoo2/LastLetter.java
+++ w13_kontrolltoo2/LastLetter.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 import java.io.*;
  
 public class LastLetter {
@@ -10,8 +12,7 @@ public class LastLetter {
  
     File file = new File("tekst.txt");
     Scanner input = new Scanner(file); 
-    int count = 0;
-    ArrayList<Character> last_ch = new ArrayList<Character>();
+    ArrayList<Character> last_ch = new ArrayList<Character>(); 
 
     //Leian sõnade viimased tähed ja lisan neid arraylisti
     while(input.hasNext()) {
@@ -21,21 +22,82 @@ public class LastLetter {
         last_ch.add(word.charAt(word.length()-2));
     }else{
         last_ch.add(last);
+      }
     }
-    count = count + 1;
-    }
-    //Trükin välja sõnade viimased tähed (arraylist)
-    System.out.println("Tähed:");
-    System.out.println(last_ch);
-    //Trükin välja letters cont (seda polnud ülesandes, tegin lihtsalt enda jaoks)
-    System.out.println("Tähtede arv: " + count);
 
-    //Leian erinevate sõnalõputähtede esinemissagedust ja trükin neid esinemissageduse järjekorras
-    System.out.println("Sõnalõputähtede esinemissagedus:");
-    Set<Character> uniguech = new HashSet<Character>(last_ch);
-    for(Character i : uniguech){
-      System.out.println(i + ": " + Collections.frequency(last_ch, i));
+    //Trükin välja sõnade viimased tähed (arraylist)
+    System.out.println("Sõnalõputähed:");
+    System.out.println(last_ch + "\n");
+
+    //Leian erinevate sõnalõputähtede esinemissagedust
+    String s = freqSort(arrayToString(last_ch));
+    int[] freq = new int[s.length()];    
+    int i;
+    int j; 
+    char string[] = s.toCharArray();
+
+    for(i = 0; i < s.length(); i++){    
+      freq[i] = 1;    
+      for(j = i + 1; j < s.length(); j++){    
+        if(string[i] == string[j]){    
+          freq[i]++;    
+          string[j] = '0';    
+        }
+      }
     }
+
+    //trükin välja sõnalõputähed esinemissageduse järjekorras
+    System.out.println("Sõnalõputähed ja nende esinemissagedus:");  
+
+    for(i = 0; i < freq.length; i++){    
+      if(string[i] != '0'){   
+      System.out.println(string[i] + ": " + freq[i]);   
+      }
+    }  
     input.close();
+  }
+
+  //teen sõnalõputähtede arraylistist stringi
+  public static String arrayToString(ArrayList<Character> list){    
+    StringBuilder b = new StringBuilder(list.size());
+    for(Character c : list){
+      b.append(c);
     }
+    return b.toString();
+  }
+
+  //sorteerin saadud stringi esinemissageduse järgi
+  public static String freqSort(String s){    
+    List<Set<Character>> cByFreq = new ArrayList<>();
+    Map<Character, Integer> freqMap = new HashMap<>(); 
+    cByFreq.add(null); 
+
+    for(char c : s.toCharArray()){
+      Character ch = c;
+      int oldFreq = freqMap.getOrDefault(c, 0);
+
+      if(oldFreq != 0){
+        cByFreq.get(oldFreq).remove(ch);
+      }
+      
+      int newFreq = oldFreq + 1;
+
+      if(newFreq >= cByFreq.size()){
+        cByFreq.add(new TreeSet());
+      }
+
+      cByFreq.get(newFreq).add(ch);
+      freqMap.put(ch, newFreq);
+    }
+
+    StringBuilder result = new StringBuilder();
+    for(int i = 1; i < cByFreq.size(); i++){ 
+      Set<Character> chSet = cByFreq.get(i); 
+      for(Character c : chSet){ 
+        for (int j = 0; j < i; j++) result.append(c);
+      }
+    }
+    result.reverse();
+    return result.toString();
+  }
 }
